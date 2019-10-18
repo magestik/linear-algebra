f5ccbdfee5b1fdee4672550e395136ee7a802447
magestik <bastien@magestik.fr>
2019-10-18 11:23:24 +0200

    More unit tests

--- test/vector.cpp
+++ test/vector.cpp
@@ -2,64 +2,19 @@
 
 #include <assert.h>
 
-/**
- * @brief test_float_min3
- */
-void test_float_min3(void)
+template<typename T>
+void test_vector2_dot()
 {
-	float x = 1.0f;
-	float y = 2.0f;
-	float z = 3.0f;
-
-	assert(min3(x, y, z) == 1.0f);
-	assert(min3(x, z, y) == 1.0f);
-	assert(min3(y, x, z) == 1.0f);
-	assert(min3(y, z, x) == 1.0f);
-	assert(min3(z, x, y) == 1.0f);
-	assert(min3(z, y, x) == 1.0f);
+	vector2<T> a;
+	vector2<T> b;
+	dot(a, b);
 }
 
-/**
- * @brief test_float_max3
- */
-void test_float_max3(void)
+template<typename T>
+void test_vector2_normalize()
 {
-	float x = 1.0f;
-	float y = 2.0f;
-	float z = 3.0f;
-
-	assert(max3(x, y, z) == 3.0f);
-	assert(max3(x, z, y) == 3.0f);
-	assert(max3(y, x, z) == 3.0f);
-	assert(max3(y, z, x) == 3.0f);
-	assert(max3(z, x, y) == 3.0f);
-	assert(max3(z, y, x) == 3.0f);
-}
-
-/**
- * @brief test_float_med3
- */
-void test_float_med3(void)
-{
-	float x = 1.0f;
-	float y = 2.0f;
-	float z = 3.0f;
-
-	assert(med3(x, y, z) == 2.0f);
-	assert(med3(x, z, y) == 2.0f);
-	assert(med3(y, x, z) == 2.0f);
-	assert(med3(y, z, x) == 2.0f);
-	assert(med3(z, x, y) == 2.0f);
-	assert(med3(z, y, x) == 2.0f);
-
-	assert(med3(y, y, x) == 2.0f);
-	assert(med3(y, y, z) == 2.0f);
-	assert(med3(y, x, y) == 2.0f);
-	assert(med3(y, z, y) == 2.0f);
-	assert(med3(x, y, y) == 2.0f);
-	assert(med3(z, y, y) == 2.0f);
-
-	assert(med3(y, y, y) == 2.0f);
+	vector2<T> a;
+	normalize(a);
 }
 
 /**
@@ -68,9 +23,8 @@ void test_float_med3(void)
  */
 int main(void)
 {
-	test_float_min3();
-	test_float_max3();
-	test_float_med3();
+	test_vector2_dot<float>();
+	test_vector2_normalize<float>();
 
 	return 0;
 }
