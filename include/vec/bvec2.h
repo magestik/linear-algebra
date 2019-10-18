78eb44bdcd7dbf320ebbcc6810a97c972364324c
magestik <bastien@magestik.fr>
2019-10-18 17:40:41 +0200

    coding standard

--- include/vec/bvec2.h
+++ include/vec/bvec2.h
@@ -8,11 +8,11 @@ struct bvec2
 
 public:
 
-	inline explicit bvec2 (void) : bitset(0) { }
+	explicit bvec2 (void) : bitset(0) { }
 
-	inline explicit bvec2 (int v) : bitset(v & 0x3) { }
+	explicit bvec2 (int v) : bitset(v & 0x3) { }
 
-	inline explicit bvec2 (bool b1, bool b2)
+	explicit bvec2 (bool b1, bool b2)
 	{
 		bitset = ((b1 ? 0x1 : 0) | (b2 ? 0x2 : 0));
 	}
