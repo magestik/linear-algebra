6e165390cf88d8b42b5038a0da6f76d9b539aed3
Bastien <bastien@magestik.fr>
2019-11-15 20:30:52 +0100

    Update CI scripts

--- ci/appveyor.cmake
+++ ci/appveyor.cmake
@@ -8,17 +8,24 @@ set(APPVEYOR_BUILD_WORKER_IMAGE "$ENV{APPVEYOR_BUILD_WORKER_IMAGE}")
 if (APPVEYOR_BUILD_WORKER_IMAGE STREQUAL "Visual Studio 2015")
 	set(CTEST_CMAKE_GENERATOR "Visual Studio 14 2015")
 	set(CTEST_BUILD_NAME "windows-vs14-ide")
+elseif (APPVEYOR_BUILD_WORKER_IMAGE STREQUAL "Visual Studio 2017")
+	set(CTEST_CMAKE_GENERATOR "Visual Studio 15 2017")
+	set(CTEST_BUILD_NAME "windows-vs15-ide")
+elseif (APPVEYOR_BUILD_WORKER_IMAGE STREQUAL "Visual Studio 2019")
+	set(CTEST_CMAKE_GENERATOR "Visual Studio 16 2019")
+	set(CTEST_BUILD_NAME "windows-vs16-ide")
 endif()
 
 set(CTEST_BUILD_CONFIGURATION "Debug")
-#set(CTEST_BUILD_OPTIONS "")
 
 set(WITH_MEMCHECK FALSE)
 set(WITH_COVERAGE FALSE)
 
+find_program(CTEST_GIT_COMMAND NAMES git)
+
 ctest_start(Continuous)
 
-#ctest_update()
+ctest_update()
 
 ctest_configure()
 
