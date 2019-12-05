075dbe115ff393e8cea3b6aa1d117a0ac7030d1b
Bastien <bastien@magestik.fr>
2019-12-06 00:21:18 +0100

    improve CI scripts

--- ci/travis.cmake
+++ ci/travis.cmake
@@ -24,22 +24,4 @@ find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
 
 set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND}")
 
-ctest_start(Continuous)
-
-ctest_update()
-
-ctest_configure(OPTIONS "${CTEST_BUILD_OPTIONS}")
-
-ctest_build()
-
-ctest_test()
-
-if (WITH_COVERAGE AND CTEST_COVERAGE_COMMAND)
-  ctest_coverage()
-endif (WITH_COVERAGE AND CTEST_COVERAGE_COMMAND)
-
-if (WITH_MEMCHECK AND CTEST_MEMORYCHECK_COMMAND)
-  ctest_memcheck()
-endif (WITH_MEMCHECK AND CTEST_MEMORYCHECK_COMMAND)
-
-ctest_submit()
+include(${CTEST_SCRIPT_DIRECTORY}/common.cmake)
