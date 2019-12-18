5bf9bd47f836fac679b6e91850d24a47c5276998
Bastien <bastien@magestik.fr>
2019-12-18 17:35:48 +0100

    Start iOS/Android CI config

--- ci/github.cmake
+++ ci/github.cmake
@@ -3,20 +3,41 @@ set(CTEST_BINARY_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/build")
 
 set(CTEST_SITE "github.com")
 
+set(CTEST_BUILD_OPTIONS "")
+
+if (NOT DEFINED ANDROID)
+	set(ANDROID 0)
+endif()
+
+if (NOT DEFINED IOS)
+	set(IOS 0)
+endif()
+
 if (GITHUB_RUNNER_OS STREQUAL "Linux")
 
-	set(CXX "$ENV{CXX}")
-	if (CXX STREQUAL "g++")
-		set(CTEST_BUILD_NAME "linux-gcc-default")
-	elseif (CXX STREQUAL "clang++")
-		set(CTEST_BUILD_NAME "linux-gcc-default")
+	if (ANDROID)
+		set(CTEST_BUILD_NAME "android-gcc-default")
+		set(CTEST_BUILD_OPTIONS "-DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a -DCMAKE_ANDROID_STL_TYPE=system")
+	else()
+		set(CXX "$ENV{CXX}")
+		if (CXX STREQUAL "g++")
+			set(CTEST_BUILD_NAME "linux-gcc-default")
+		elseif (CXX STREQUAL "clang++")
+			set(CTEST_BUILD_NAME "linux-gcc-default")
+		endif()
 	endif()
 
 	set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 
 elseif (GITHUB_RUNNER_OS STREQUAL "macOS")
 
-	set(CTEST_BUILD_NAME "osx-xcode-ide")
+	if (IOS)
+		set(CTEST_BUILD_NAME "ios-xcode-ide")
+		set(CTEST_BUILD_OPTIONS "-DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_ARCHITECTURES=armv7;armv7s;arm64;i386;x86_64")
+	else()
+		set(CTEST_BUILD_NAME "osx-xcode-ide")
+	endif()
+
 	set(CTEST_CMAKE_GENERATOR "Xcode")
 
 elseif (GITHUB_RUNNER_OS STREQUAL "Windows")
@@ -29,7 +50,6 @@ endif()
 set(CTEST_BUILD_CONFIGURATION "Debug")
 
 set(WITH_MEMCHECK TRUE)
-set(CTEST_BUILD_OPTIONS "")
 set(WITH_COVERAGE FALSE)
 
 find_program(CTEST_GIT_COMMAND NAMES git)
